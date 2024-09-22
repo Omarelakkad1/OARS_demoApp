22c564dac10e15bb12ab2bd5bfba8efe79974e70
yukasyah <Ukasyah2003@gmail.com>
2024-09-22 19:50:07 +0800

    fixed and cleaned navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,16 +18,19 @@ class Navbar extends Component {
                 </div>
 
                 <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-                    {Navmenuitems.map((item, index) => (
-                        <li key={index}>
-                            <Link to={item.url} className={item.cName}>
-                                <i className={item.icon}></i>
-                                {item.title}
-                            </Link>
-                        </li>
-                    ))}
-                    <li>
-                    </li>
+                    {Navmenuitems.map((item, index) => {
+                        if (item.title !== "") {
+                            return (
+                                <li key={index}>
+                                    <Link to={item.url} className={item.cName}>
+                                        <i className={item.icon}></i>
+                                        {item.title}
+                                    </Link>
+                                </li>
+                            );
+                        }
+                        return null;
+                    })}
                 </ul>
             </nav>
         );
