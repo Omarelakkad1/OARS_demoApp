d8522a7ddf82038cb07aede69fe30928c0549842
yukasyah <Ukasyah2003@gmail.com>
2024-09-22 18:42:20 +0800

    added more events

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -18,6 +18,48 @@ const EventList = [
       description: "This is the third Event.",
       link: "Events3.php",
     },
+    {
+      id: 4,
+      title: "Events 4",
+      description: "This is the fourth Event.",
+      link: "Events4.php",
+    },
+    {
+      id: 5,
+      title: "Events 5",
+      description: "This is the fifth Event.",
+      link: "Events5.php",
+    },
+    {
+      id: 6,
+      title: "Events 6",
+      description: "This is the sixth Event.",
+      link: "Events6.php",
+    },
+    {
+      id: 7,
+      title: "Events 7",
+      description: "This is the seventh Event.",
+      link: "Events7.php",
+    },
+    {
+      id: 8,
+      title: "Events 8",
+      description: "This is the eighth Event.",
+      link: "Events8.php",
+    },
+    {
+      id: 9,
+      title: "Events 9",
+      description: "This is the ninth Event.",
+      link: "Events9.php",
+    },
+    {
+      id: 10,
+      title: "Events 10",
+      description: "This is the tenth Event.",
+      link: "Events10.php",
+    },
     // Add more events as needed
   ];
   
